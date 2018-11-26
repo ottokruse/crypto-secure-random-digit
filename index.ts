4c713039645bf341fb0bb3002ac5464d05c0a851
Otto Kruse <ottokruse@gmail.com>
2018-11-26 14:17:37 +0100

    improved doc

--- index.ts
+++ index.ts
@@ -7,8 +7,8 @@ export function randomDigit() {
         // If the random number is 200 or bigger - disregard it and try another random byte
         // This is because we do '% 10' later on, which would be biassed otherwise
         // See explanation: https://gist.github.com/joepie91/7105003c3b26e65efcea63f3db82dfba
-        // A random byte has 255 possible values, as long as we stick to 0 - 199 we can be sure
-        // the distribution of possible last digits (which you get by % 10) is evenly spread
+        // A random byte has 256 possible values, as long as we stick to the first 200 (that is 0 - 199 inclusive)
+        // we can be sure the distribution of possible last digits (which you get by % 10) is evenly spread
         if (randomNumber >= 200) {
             continue;
         }
